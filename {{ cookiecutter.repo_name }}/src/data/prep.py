6c16a09e2047069a012c91672eca730dd8e9728f
Remy Lagrois <r.lagrois@gmail.com>
2019-01-29 11:49:00 -0500

    Create prep.py

---
+++ {{ cookiecutter.repo_name }}/src/data/prep.py
@@ -0,0 +1,17 @@
+""" 
+Remy Lagrois
+"""
+
+import pandas as pd
+import os
+
+def test():
+    print('prep works')
+    
+
+
+
+
+def available():
+    print('Available data.prep Functions:')
+    print('test() - function to check module is properly imported')
